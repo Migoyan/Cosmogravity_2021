65ae1454bd5f12ca718da54fc39b1d064450c0ff
You3419 <you3419@live.fr>
2021-03-26 16:06:33 +0100

    fonction pause

--- js/Simulation_Trajectoires/zoom.js
+++ js/Simulation_Trajectoires/zoom.js
@@ -1,2 +1,25 @@
 /* Fichier pour rasemble les fonction du zoom et autre bouton des simulation */
 
+
+
+// Fonction bouton pause
+// tirer du dossier calcul_trajectoire_inter_photon
+function pausee(compteur,mobile,mobilefactor) {
+
+    if (mobile.pause == false) {
+        mobile.pause = true;
+        document.getElementById("pau").src = "Images/lecture.png";
+        document.getElementById("pau").title = texte.pages_trajectoire.bouton_lecture;
+        document.getElementById("indic_calculs").innerHTML = texte.pages_trajectoire.calcul_enpause;
+        clearInterval(mobile.myInterval);
+    } 
+    else {
+      if(mobile.peuxonrelancer == true) {
+      mobile.pause = false;
+      document.getElementById("indic_calculs").innerHTML = texte.pages_trajectoire.calcul_encours;
+      document.getElementById("pau").title = texte.pages_trajectoire.bouton_pause;
+      document.getElementById("pau").src = "Images/pause.png";
+      mobile.myInterval = setInterval(animate.bind(null,compteur,mobile,mobilefactor), 10/6);
+      }
+    }
+  }
