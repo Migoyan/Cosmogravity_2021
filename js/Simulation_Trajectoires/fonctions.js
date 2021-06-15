f0fe3b4aeee97c6ec4ce29a42bfd4afa25477d37
You3419 <you3419@live.fr>
2021-06-15 16:28:04 +0200

    netoyage et commentaire

--- js/Simulation_Trajectoires/fonctions.js
+++ js/Simulation_Trajectoires/fonctions.js
@@ -44,16 +44,16 @@ var calculs = calculs || (function() {
          */
         ///Calcule la vitess réel en m/s pour la métrique de Schwarzchild pour une intéraction non barionique à l'intérieur de l'astre
         MSC_In_vitess : function(E,L,r,rs,ra,photon){
-            alpha=1-((r**2)*rs)/(ra**3);
-            beta=(3/2)*Math.sqrt(1-rs/ra)-(1/2)*Math.sqrt(alpha);
-            vphi=Math.sqrt((r**2/bt**2)*(c*L*beta**2/r**2)**2);
+            alpha_=1-((r**2)*rs)/(ra**3); /// alpha pour la métrique de Schawr interne
+            beta_=(3/2)*Math.sqrt(1-rs/ra)-(1/2)*Math.sqrt(alpha_);/// beta pour la métrique de Schawr interne
+            vphi=Math.sqrt((r**2/bt**2)*(c*L*beta_**2/r**2)**2);
             if(photon){
-                dr=((c/E)**2)*alpha*(bt**4)*((E/beta)**2-(L/r)**2);
-                vr=Math.sqrt(dr/(alpha*beta**2));
+                dr=((c/E)**2)*alpha_*(bt**4)*((E/beta_)**2-(L/r)**2);
+                vr=Math.sqrt(dr/(alpha_*beta_**2));
             }
             else{
-                dr=((c/E)**2)*alpha*(beta**4)*((E/beta)**2-(L/r)**2-1);
-                vr=Math.sqrt(dr/(alpha*beta**2));
+                dr=((c/E)**2)*alpha_*(beta_**4)*((E/beta_)**2-(L/r)**2-1);
+                vr=Math.sqrt(dr/(alpha_*beta_**2));
             }
             vtot=Math.sqrt(vphi**2+vr**2);
             
@@ -72,23 +72,23 @@ var calculs = calculs || (function() {
          */
         MK_vitess :function(E,l,a,r,rs,photon){
             //alert(a);
-            delta=(r**2)-(rs*r)+(a**2); ///delta dans la metric de kerr
+            delta_=(r**2)-(rs*r)+(a**2); ///delta dans la metric de kerr
             dphi=c*((rs*a*E)/r+(1-rs/r)*l)/((r**2+a**2+(rs/r)*(a**2))*E-rs*a*l/r);
-            vphie=delta*(dphi**2)/((1-(rs/r)+rs*a*(dphi)/(c*r))**2);
+            vphie=delta_*(dphi**2)/((1-(rs/r)+rs*a*(dphi)/(c*r))**2);
             vphie=Math.sqrt(vphie);
 
             if(photon){
                 dr=(c**2)*(E**2+((a**2)*(E**2)-l**2)/(r**2)+rs*(((l-a*E)**2)/(r**3)));
-                dr*=(delta**2)/(((r**2+a**2+(rs/r)*a**2)*E-rs*a*l/r)**2);
-                vr=(1-rs/r)*((r**2)*dr/delta)/(1-(rs/r)+rs*a*(dphi)/(c*r))**2;
+                dr*=(delta_**2)/(((r**2+a**2+(rs/r)*a**2)*E-rs*a*l/r)**2);
+                vr=(1-rs/r)*((r**2)*dr/delta_)/(1-(rs/r)+rs*a*(dphi)/(c*r))**2;
                 vr=Math.sqrt(Math.abs(vr));
                 //alert(vr);
                 
             }
             else{
                 dr=(c**2)*(E**2-1+(rs/r)+((a**2)*(E**2-1)-l**2)/(r**2)+rs*(((l-a*E)**2)/(r**3)));
-                dr*=(delta**2)/((r**2+a**2+(rs/r)*a**2)*E-rs*a*l/r)**2;
-                vr=(1-rs/r)*((r**2)*dr/delta)/((1-(rs/r)+rs*a*(dphi)/(c*r))**2);
+                dr*=(delta_**2)/((r**2+a**2+(rs/r)*a**2)*E-rs*a*l/r)**2;
+                vr=(1-rs/r)*((r**2)*dr/delta_)/((1-(rs/r)+rs*a*(dphi)/(c*r))**2);
                 vr=Math.sqrt(Math.abs(vr));
             }
             vtot=Math.sqrt(vr**2+vphie**2);
